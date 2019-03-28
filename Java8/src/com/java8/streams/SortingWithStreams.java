e7f97aa82cfa59f24aa74d5848fd2a6d160e496e
TejaswiV <tejaswi.venupalli@gmail.com>
2019-03-28 14:45:09 +1100

    updated

--- Java8/src/com/java8/streams/SortingWithStreams.java
+++ Java8/src/com/java8/streams/SortingWithStreams.java
@@ -16,7 +16,8 @@ public class SortingWithStreams {
 				new Employee(3, "Mark", 3200.0),
 				new Employee(4, "Max", 1000.0),
 				new Employee(5, "Dan", 5000.0),
-				new Employee(6, "Sam", 4200.0));
+				new Employee(6, "Sam", 4200.0),
+				new Employee(7, "Dan", 8000.0));
 		
 		list.stream()
 			.map(Employee::getName)
@@ -27,9 +28,18 @@ public class SortingWithStreams {
 		.sorted(Comparator.comparing(Employee::getSalary).reversed())
 		.forEach(System.out::println);
 		
+		System.out.println();
+		
 		list.stream()
 		.sorted(Comparator.comparing(Employee::getName).thenComparing(Comparator.comparing(Employee::getSalary)))
 		.forEach(System.out::println);
+		
+		list.stream()
+			.map(Employee::getName)
+			.distinct()
+			.sorted()
+			.forEach(System.out::println);
+		
 	}
 
 }
