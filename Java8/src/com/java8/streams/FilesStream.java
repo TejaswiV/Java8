246969ccb8b06541f645ea59a794257a35c16bc3
TejaswiV <tejaswi.venupalli@gmail.com>
2019-03-29 12:58:27 +1100

    Using Collectors

--- Java8/src/com/java8/streams/FilesStream.java
+++ Java8/src/com/java8/streams/FilesStream.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.java8.streams.helper.DvdInfo;
 
@@ -23,11 +24,24 @@ public class FilesStream {
 		
 		try {
 			
-		Files.lines(Paths.get(path))
+		/*Files.lines(Paths.get(path))
 			.forEach(line -> {
 				String[] info = line.split(" / ");
 				list.add(new DvdInfo(info[0], info[1], info[2]));				
-			});
+			}); */
+			
+			
+			list = Files.lines(Paths.get(path))
+					.map(line -> {
+						String[] info = line.split(" / ");
+						return new DvdInfo(info[0], info[1], info[2]);		
+					})
+					.collect(Collectors.toList());
+			
+		/*list = Files.lines(Paths.get(path))
+				.map(line -> new DvdInfo(line.split(" / ")[0], line.split(" / ")[1], line.split(" / ")[2]))
+				.collect(Collectors.toList());
+		*/	
 			
 		} catch (IOException e) {
 			
